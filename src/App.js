573c715381c50201b16b362c063db58e0e7c76da
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-04 20:10:55 -0400

    added pages and links in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import './App.css';
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <HomePage />
+      <AboutPage />
+      <Project />
+      <Contact />
 
     </div>
   );
