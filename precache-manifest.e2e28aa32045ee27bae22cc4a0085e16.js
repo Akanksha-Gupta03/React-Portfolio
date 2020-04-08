f923cbd36ddd0ea4342dede167ff5772377843d3
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-08 11:07:42 -0400

    Updates

---
+++ precache-manifest.e2e28aa32045ee27bae22cc4a0085e16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52d105acffea45fdbe670ff0613447e",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "538b4905d359cbcca8b7",
+    "url": "/React-Portfolio/static/css/main.c9f2ad2f.chunk.css"
+  },
+  {
+    "revision": "cc44ec8bfaedee66b011",
+    "url": "/React-Portfolio/static/js/2.77d09407.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.77d09407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538b4905d359cbcca8b7",
+    "url": "/React-Portfolio/static/js/main.bdd5fc42.chunk.js"
+  },
+  {
+    "revision": "5d16534d7724865583f3",
+    "url": "/React-Portfolio/static/js/runtime-main.2316dad8.js"
+  },
+  {
+    "revision": "539c510e069b0c80c0439eb43e137d6a",
+    "url": "/React-Portfolio/static/media/me.539c510e.JPG"
+  }
+]);
