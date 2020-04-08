e141b262b22981363a9abea866c5a67045d4ab9c
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-08 09:33:35 -0400

    modified css

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -11,7 +11,7 @@ function AboutPage(){
           <div class="col-lg-6 container about text-center">
               <h2 class="aboutHeading">ABOUT ME</h2>
               <p class="para">Hi! I'm a web developer actively seeking for opportunity to work in a challenging and responsibility-driven environment under little supervision,aspire to make your company successful and make your customers happier.</p>
-              <p class="para">Besides coding ,I like to spend my time learning new technology</p>
+              <p class="para">Besides coding ,I like to travelling across the world and also spending my time learning new technology.</p>
           </div>
         </div>
  )
